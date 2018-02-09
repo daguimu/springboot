2ccc889c722042ba59633206383f859a74d39da0
daguimu <30684111+daguimu@users.noreply.github.com>
2018-02-09 09:38:20 +0800

    Update HelloController.java

--- springboot0106/src/main/java/com/boot/controller/HelloController.java
+++ springboot0106/src/main/java/com/boot/controller/HelloController.java
@@ -19,47 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @Autowired
     PeopleService peopleService;
-    @RequestMapping(value = "/add")
-    public PeopleModel add(){
-        RoleModel role=new RoleModel();
-        role.setRoleName("teacher");
-        DeptModel dept=new DeptModel();
-        dept.setDeptName("java");
-        PeopleModel p=new PeopleModel();
-        p.setPeopleAge(18);
-        p.setPeopleEmail("171@qq.com");
-        p.setPeopleTel("18445853593");
-        p.setRole(role);
-        p.setPeopleName("daguimu");
-        p.setDept(dept);
-        peopleService.save(p);
-        return p;
-    }
 
     @RequestMapping(value = "/gitpdf")
     public String gitpdf(@RequestParam(value = "path") String path,@RequestParam(value = "width") Integer width,@RequestParam(value = "hight") Integer hight,@RequestParam(value = "dpi") Integer dpi,@RequestParam(value = "type") String type){
         PdfReader.parsePdf(path,width,hight,dpi,type);
         return "gitpdf";
     }
-
-
-    @RequestMapping(value = "htmt")
-    public  String html(@RequestParam(value = "url" )String url){
-        try{
-            HtmlToImage.generateOutput(url);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return  "lposda";
-    }
-    @RequestMapping(value = "htt")
-    public  String htt(@RequestParam(value = "url" )String url){
-        try{
-            HtT.pop(url);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return  "lposda";
-    }
-
 }
